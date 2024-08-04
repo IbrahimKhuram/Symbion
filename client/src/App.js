00ceaa1d70a306d77a075b49721f5c9ac7537848
IbrahimKhuram <113419133+IbrahimKhuram@users.noreply.github.com>
2024-08-04 15:51:50 +0500

    All contract f() integrated; errors remaining
    Errors to solve:
    
    - Non-owner acc can delete owner's project
    - Non-owner acc can withdraw another owners project
    - Potentially more

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 
-
 const ABI = [
   {
     "inputs": [],
@@ -418,15 +417,31 @@ const ABI = [
     "stateMutability": "nonpayable",
     "type": "function"
   }
-];
-
-
+]
 
 const App = () => {
+
+  const [contractBalance, setContractBalance] = useState('');
+
+  const [projectId_getProject, setProjectId_getProject] = useState('');
+  const [projectDetails, setProjectDetails] = useState(null);
+
+  const [projectId_deleteProject, setProjectId_deleteProject] = useState('');
+
   const [projects, setProjects] = useState([]);
   const [projectWallet, setProjectWallet] = useState('');
   const [goalAmount, setGoalAmount] = useState('');
   const [deadline, setDeadline] = useState('');
+
+  const [projectId_deposit_fundraising, setProjectId_deposit_fundraising] = useState('');
+  const [depositAmount_fundraising, setDepositAmount_fundraising] = useState('');
+
+  const [projectId_deposit_crowdfunding, setProjectId_deposit_crowdfunding] = useState('');
+  const [depositAmount_crowdfunding, setDepositAmount_crowdfunding] = useState('');
+  
+  const [projectId_withdrawal, setProjectId_withdrawal] = useState('');
+  const [withdrawAmount, setWithdrawAmount] = useState('');
+
   const [provider, setProvider] = useState(null);
   const [signer, setSigner] = useState(null);
   const [contract, setContract] = useState(null);
@@ -436,7 +451,7 @@ const App = () => {
   useEffect(() => {
     const init = async () => {
       if (window.ethereum) {
-        const provider = new ethers.BrowserProvider(window.ethereum);
+        const provider = new ethers.JsonRpcProvider('http://localhost:8545');
         const signer = await provider.getSigner();
         const contract = new ethers.Contract(contractAddress, ABI, signer);
         setProvider(provider);
@@ -448,7 +463,52 @@ const App = () => {
     init();
   }, []);
 
+  const fetchContractBalance = async () => {
+    if (!provider || !contractAddress) return;
+    const balance = await provider.getBalance(contractAddress);
+    setContractBalance(ethers.formatEther(balance));
+  };
+
+  const addProject = async () => {
+    if (!contract) return;
+    const tx = await contract.addProject(projectWallet, ethers.parseEther(goalAmount), deadline);
+  };
+
+  const deleteProject = async () => {
+    if (!contract) return;
+    const tx = await contract.deleteProject(projectId_deleteProject);
+  };
+
+  const depositFundsFundraising = async () => {
+    if (!contract || !projectId_deposit_fundraising || !depositAmount_fundraising) return;
+    await contract.depositFunds_Fundraising(projectId_deposit_fundraising, { value: ethers.parseEther(depositAmount_fundraising) });
+  };
+
+  const depositFundsCrowdfunding = async () => {
+    if (!contract || !projectId_deposit_crowdfunding || !depositAmount_crowdfunding) return;
+    await contract.depositFunds_Crowdfunding(projectId_deposit_crowdfunding, { value: ethers.parseEther(depositAmount_crowdfunding) });
+  };
+
+  const withdrawFunds = async () => {
+    if (!contract || !projectId_withdrawal || !withdrawAmount) return;
+    await contract.withdrawFunds(projectId_withdrawal, ethers.parseEther(withdrawAmount));
+  };
+
+  const fetchProject = async () => {
+    if (!contract || !projectId_getProject) return;
+    const projectData = await contract.getProject(projectId_getProject);
+    const formattedProject = {
+      projectId: Number(projectData[0]),
+      projectWallet: projectData[1],
+      goalAmount: ethers.formatEther(projectData[2]),
+      amountRaised: ethers.formatEther(projectData[3]),
+      deadline: Number(projectData[4])
+    };
+    setProjectDetails(formattedProject);
+  };
+
   const fetchProjects = async (contract) => {
+    if (!contract) return;
     const projectsData = await contract.getAllProjects();
     console.log(projectsData[3]);
     const formattedProjects = projectsData[0].map((_, i) => ({
@@ -461,40 +521,139 @@ const App = () => {
     setProjects(formattedProjects);
   };
 
-  const addProject = async () => {
-    if (!contract) return;
-    const tx = await contract.addProject(projectWallet, ethers.parseEther(goalAmount), deadline);
-    await tx.wait();
-    await fetchProjects(contract);
-  };
-
   return (
     <div>
-      <h1>Donation Manager</h1>
+      <h1>Symbion</h1>
+      <br></br>
+
+      <div>
+        <h2>Contract Balance</h2>
+        <button onClick={fetchContractBalance}>Fetch Balance</button>
+        {contractBalance && <p>Contract Balance: {contractBalance} ETH</p>}
+      </div><br></br>
+
       <div>
         <h2>Add Project</h2>
+        <label>projectWallet: </label>
         <input
           type="text"
-          placeholder="Project Wallet"
+          placeholder="address"
           value={projectWallet}
           onChange={(e) => setProjectWallet(e.target.value)}
-        />
+        /><br></br>
+        <label>goalAmount: </label>
         <input
           type="text"
-          placeholder="Goal Amount (ETH)"
+          placeholder="uint 256"
           value={goalAmount}
           onChange={(e) => setGoalAmount(e.target.value)}
-        />
+        /><br></br>
+        <label>deadline: </label>
         <input
           type="text"
-          placeholder="Deadline"
+          placeholder="uint 256"
           value={deadline}
           onChange={(e) => setDeadline(e.target.value)}
         />
-        <button onClick={addProject}>Add Project</button>
-      </div>
+        <button onClick={addProject}>transact</button>
+      </div><br></br>
+
+      <div>
+        <h2>Delete Project</h2>
+        <label>projectId: </label>
+        <input
+          type="text"
+          placeholder="uint 256"
+          value={projectId_deleteProject}
+          onChange={(e) => setProjectId_deleteProject(e.target.value)}
+        /><br></br>
+        <button onClick={deleteProject}>transact</button>
+      </div><br></br>
+
+      <div>
+        <h2>Deposit Funds - Fundraising</h2>
+        <label>Project ID: </label>
+        <input
+          type="text"
+          placeholder="uint 256"
+          value={projectId_deposit_fundraising}
+          onChange={(e) => setProjectId_deposit_fundraising(e.target.value)}
+        /><br></br>
+        <label>Amount: </label>
+        <input
+          type="text"
+          placeholder="ETH"
+          value={depositAmount_fundraising}
+          onChange={(e) => setDepositAmount_fundraising(e.target.value)}
+        /><br></br>
+        <button onClick={depositFundsFundraising}>Fundraising</button>
+      </div><br></br>
+
+      <div>
+        <h2>Deposit Funds - Crowdfunding</h2>
+        <label>Project ID: </label>
+        <input
+          type="text"
+          placeholder="uint 256"
+          value={projectId_deposit_crowdfunding}
+          onChange={(e) => setProjectId_deposit_crowdfunding(e.target.value)}
+        /><br></br>
+        <label>Amount: </label>
+        <input
+          type="text"
+          placeholder="ETH"
+          value={depositAmount_crowdfunding}
+          onChange={(e) => setDepositAmount_crowdfunding(e.target.value)}
+        /><br></br>
+        <button onClick={depositFundsCrowdfunding}>Crowdfunding</button>
+      </div><br></br>
+
+      <div>
+        <h2>Withdraw Funds</h2>
+        <label>Project ID: </label>
+        <input
+          type="text"
+          placeholder="uint 256"
+          value={projectId_withdrawal}
+          onChange={(e) => setProjectId_withdrawal(e.target.value)}
+        /><br></br>
+        <label>Amount: </label>
+        <input
+          type="text"
+          placeholder="ETH"
+          value={withdrawAmount}
+          onChange={(e) => setWithdrawAmount(e.target.value)}
+        /><br></br>
+        <button onClick={withdrawFunds}>Withdraw</button>
+      </div><br></br>
+
+      <div>
+        <h2>Get Project by ID</h2>
+        <label>Project ID: </label>
+        <input
+          type="text"
+          placeholder="uint 256"
+          value={projectId_getProject}
+          onChange={(e) => setProjectId_getProject(e.target.value)}
+        /><br></br>
+        <button onClick={fetchProject}>Fetch Project</button>
+        <br></br><br></br>
+
+        {projectDetails && (
+          <div>
+            <h3>Project Details</h3>
+            <p>Project ID: {projectDetails.projectId}</p>
+            <p>Project Wallet: {projectDetails.projectWallet}</p>
+            <p>Goal Amount: {projectDetails.goalAmount} ETH</p>
+            <p>Deadline: {projectDetails.deadline}</p>
+            <p>Amount Raised: {projectDetails.amountRaised} ETH</p>
+          </div>
+        )}
+      </div><br></br>
+
       <div>
         <h2>All Projects</h2>
+        <button onClick={fetchProjects}>transact</button>
         <ul>
           {projects.map((project) => (
             <li key={project.projectId}>
